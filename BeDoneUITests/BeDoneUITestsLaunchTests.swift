b4a2ad8416e5db4da8feae09bfacf1e4b90ec414
Luodi Wang <luodiwang4@gmail.com>
2023-07-21 20:58:27 -0400

    new files

---
+++ BeDoneUITests/BeDoneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BeDoneUITestsLaunchTests.swift
+//  BeDoneUITests
+//
+//  Created by Jazil Zaim on 11/17/22.
+//
+
+import XCTest
+
+final class BeDoneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ Pods/Target Support Files/Pods-BeDoneTests/Pods-BeDoneTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BeDoneTests : NSObject
+@end
+@implementation PodsDummy_Pods_BeDoneTests
+@end
